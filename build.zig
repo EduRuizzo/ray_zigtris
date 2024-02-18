71591d2b31f9fe9e25776171f18c7f03e17a4a1d
Edu Ruiz <edu.ruizzo@gmail.com>
2024-02-18 19:46:38 +0100

    Add modules

--- build.zig
+++ build.zig
@@ -1,9 +1,10 @@
 const std = @import("std");
+const raySdk = @import("raylib/src/build.zig");
 
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -15,26 +16,36 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "ray_zigtris",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/root.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
-
     const exe = b.addExecutable(.{
         .name = "ray_zigtris",
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
+
+    const raylib = try raySdk.addRaylib(b, target, optimize, .{
+        .raudio = true,
+        .rmodels = true,
+        .rshapes = true,
+        .rtext = true,
+        .rtextures = true,
+        // Enable raygui
+        .raygui = true,
+        .platform_drm = false,
+    });
+
+    raylib.installHeader("raylib/src/raylib.h", "raylib.h");
+    // TODO: remove?
+    // raylib.installHeader("raylib/src/raymath.h", "raymath.h");
+    // raylib.installHeader("raylib/src/rlgl.h", "rlgl.h");
+
+    // I could have just copied the header, but using a git submodule is more convenient if I ever need a more recent version
+    raylib.installHeader("raygui/src/raygui.h", "raygui.h");
+    exe.linkLibrary(raylib);
+
+    // TODO: the exe needs the headers too?
+    exe.installHeader("raylib/src/raylib.h", "raylib.h");
+    exe.installHeader("raygui/src/raygui.h", "raygui.h");
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
@@ -66,26 +77,16 @@ pub fn build(b: *std.Build) void {
 
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/root.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
     const exe_unit_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
-
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
 }
